31d4b77bbd7f2e3b45daecd572ffd3fecc62efa8
Timo Peters <timopeters95@gmail.com>
2020-04-26 06:22:55 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Bing Wallpaper Changer
+
+This python script will change your Wallpaper to the most recent Bing picture of the day as soon as it is available.
+
+Only works on Linux so far.
--- bing_wallpaper_changer.py
+++ bing_wallpaper_changer.py
@@ -1,18 +1,17 @@
-
 import requests
-from bs4 import BeautifulSoup
+
 
 BING_URL = 'https://www.bing.com/'
 URL_REGEX = '<div class=\"img_cont\"style=\"background-image: url(/th?id=OHR.FalklandRockhoppers_DE-DE6067934998_1920x1080.jpg&amp;rf=LaDigue_1920x1080.jpg)\">'
-
 
 htmltext = requests.get(BING_URL).text
 
 print(htmltext)
 
-#ToDo Create Regex
-#ToDo Find a way to Download and safe the .jpg in a given directory
-#ToDo Find a way to set a Wallpaper
-#ToDo Automate downloading the newest image in a given interval
-#ToDo If the directory is bigger than 500MB -> delete the oldest image
-#ToDo Run script on Boot
+
+# ToDo Create Regex
+# ToDo Find a way to Download and safe the .jpg in a given directory
+# ToDo Find a way to set a Wallpaper
+# ToDo Automate downloading the newest image in a given interval
+# ToDo If the directory is bigger than 500MB -> delete the oldest image
+# ToDo Run script on Boot
