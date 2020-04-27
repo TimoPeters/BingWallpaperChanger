31f85326c594e6050a9d8cb391c2e2641f535524
Timo Peters <timopeters95@gmail.com>
2020-04-28 00:05:13 +0200

    Can now fetch the img from Bing and set it as Wallpaper

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ This python script will change your Wallpaper to the most recent Bing picture of
 Only works on Linux so far.
 
 ## ToDos
-- [ ] Create Regex to filter .jpg URL from binǵ.com html file
+- [x] Create Regex to filter .jpg URL from bing.com html file
 - [x] Find a way to set a Wallpaper on a Linux os
+- [ ] Create a regex to filter the image name from the url 
+- [ ] Create functions that find the newest and oldest image in the directory
 - [ ] Automate downloading the newest image in a given interval
 - [ ] If the directory is bigger than 500MB -> delete die oldest image
 - [ ] Autorun the script on boot
--- bing_wallpaper_changer.py
+++ bing_wallpaper_changer.py
@@ -1,36 +1,54 @@
 import requests
 import urllib.request
 import os
+import getpass
+import re
+
+BING_URL = 'https://bing.com'
+URL_REGEX = r'.*url\((/th.*\.jpg)'
+USER = getpass.getuser()
+WP_DIR_PATH = f'/home/{USER}/.bing_wallpapers'
+
+html_text = requests.get(BING_URL).text
+oldest_image = ''
+newest_image = ''
 
 
-BING_URL = 'https://www.bing.com/'
-URL_REGEX = '<div class=\"img_cont\"style=\"background-image: url(/th?id=OHR.FalklandRockhoppers_DE-DE6067934998_1920x1080.jpg&amp;rf=LaDigue_1920x1080.jpg)\">'
-CHANGE_WP_CMD = 'gsettings set org.gnome.desktop.background picture-uri file:///home/timo/Pictures/everaldo-coelho-2tigIl6Tt7E-unsplash.jpg'
-WP_DIR_PATH = '/home/$USER/.bing_wallpapers'
+def fetch_picture_of_the_day(url, image_safe_path):
+    urllib.request.urlretrieve(url, image_safe_path)
 
 
-def fetch_picture_of_the_day(url):
-    urllib.request.urlretrieve(url, "/home/timo/local-filename.jpg")
+def create_dir():
+    # Create a directory to safe the pictures in
+    if not os.path.exists(WP_DIR_PATH):
+        os.system('mkdir ' + WP_DIR_PATH)
+        print('Created dir')
 
 
-htmltext = requests.get(BING_URL).text
+def set_wallpaper(path):
+    print('Path: ' + path)
+    os.system(
+        f'gsettings set org.gnome.desktop.background picture-uri file://{path}')
 
 
-print(os.path.exists(WP_DIR_PATH))
+def get_image_url(regex, html):
+    pattern = re.compile(regex)
+    matcher = pattern.findall(html)
+    url = 'https://www.bing.com' + matcher[1]
+    print(url)
+    return url
 
-# Create a directory to safe the pictures in
-if not os.path.exists(WP_DIR_PATH):
-    os.system(f'mkdir ${WP_DIR_PATH}')
-    print('Created dir')
+
+def get_img_name(url):
+    # ToDo Create a regex to filter the image name from the url
+    pass
 
 
-# Change Wallpaper
-os.system(CHANGE_WP_CMD)
+create_dir()
+fetch_picture_of_the_day(get_image_url(URL_REGEX, html_text), WP_DIR_PATH + '/newest.jpg')
+set_wallpaper(WP_DIR_PATH + '/newest.jpg')
 
-fetch_picture_of_the_day("https://www.bing.com//th?id=OHR.FalklandRockhoppers_DE-DE6067934998_1920x1080.jpg&rf=LaDigue_1920x1080.jpg")
 
-# ToDo Create Regex
-# ToDo Find a way to Download and safe the .jpg in a given directory
 # ToDo Automate downloading the newest image in a given interval
 # ToDo If the directory is bigger than 500MB -> delete the oldest image
 # ToDo Run script on Boot
